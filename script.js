47698d431083d19c7853e8ac66215f6cfdcbf614
MLamb14 <54187801+MLamb14@users.noreply.github.com>
2019-10-06 23:02:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+let staffPicks = document.getElementsByClassName("Module--ListingsFromFeed");
 let feedItems = document.getElementsByClassName("feed-item");
 
+function deleteStaff () {
+    staffPicks[0].parentElement.removeChild(staffPicks[0]);
+}
+// Currently need to call this function before performing the for loop
+
 function checkText(feedIndex, wordToCheck) {
-    console.log(feedItems);
     if (feedItems[feedIndex].textContent.includes(wordToCheck)) {
-        console.log("True");
-    } else {
-        console.log("No dice");
+        feedItems[feedIndex].parentNode.removeChild(feedItems[feedIndex]);
     }
 }
 
+for (let i = 0; i < feedItems.length; i++) {
+    checkText(i, "Uniqlo");
+    console.log(i);
+}
+
 // Steps needed:
-// Add in the delete div bit after the if check
 // make a foreach loop to apply the function to all the divs 
 // test it!
