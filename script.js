7b82f5f4c5df66785f8caec59ce3d3e36b634545
MLamb14 <54187801+MLamb14@users.noreply.github.com>
2019-10-07 19:48:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 let staffPicks = document.getElementsByClassName("Module--ListingsFromFeed");
 let feedItems = document.getElementsByClassName("feed-item");
 let feedItemsLoopIndex = feedItems.length - 1; //has to be 1 less than length as the array starts at 0
+let brandToAvoid = null;
+let mainFeed = document.getElementsByClassName("feed");
+let allCapsBrand = null;
 
 function deleteStaff () {
     staffPicks[0].parentElement.removeChild(staffPicks[0]);
     feedItemsLoopIndex = feedItems.length - 1;
 }
-// Currently need to call this function before performing the for loop
+// Deletes the Staff Picks carousel so main feed can start at array index 0
 
-function checkText(feedIndex, wordToCheck) {
+function checkText(feedIndex, wordToCheck, allCapsCheck) {
     if (feedItems[feedIndex].textContent.includes(wordToCheck)) {
         feedItems[feedIndex].parentNode.removeChild(feedItems[feedIndex]);
-    }
+    } else if (feedItems[feedIndex].textContent.includes(allCapsCheck)) {
+        feedItems[feedIndex].parentNode.removeChild(feedItems[feedIndex]);
+    } 
 }
 
 function checkTextLoop () {
-    let brandToAvoid = prompt("What brand would you like to remove? Case sensitive.");
+    deleteStaff();
+    brandToAvoid = prompt("Which brand would you like to remove? (Case-sensitive, but all caps version will also be tested)");
+    allCapsBrand = brandToAvoid.toUpperCase();
     feedItemsLoopIndex = feedItems.length - 1;
     for (let i = feedItemsLoopIndex; i > -1; i--) {
-    checkText(i, brandToAvoid);
-    console.log(i);
+    checkText(i, brandToAvoid, allCapsBrand); }
+}
+
+function refresh (event) {
+    if(event.keyCode == 82){
+    feedItemsLoopIndex = feedItems.length - 1;
+    for (let i = feedItemsLoopIndex; i > -1; i--) {
+    checkText(i, brandToAvoid, allCapsBrand);
+    console.log(i); }
     }
 }
 
-// Steps needed:
-// make a foreach loop to apply the function to all the divs 
-// test it!
+document.addEventListener('keydown', refresh);
+
+checkTextLoop();
